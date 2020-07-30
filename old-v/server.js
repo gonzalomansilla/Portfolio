c9c8556a0458230a728d09f7c6e53168cb57709d
Gonzalo <gonzalom97@outlook.com>
2020-07-29 21:53:56 -0300

    Elimiacion de archivos

--- old-v/server.js
+++
@@ -1,9 +0,0 @@
-//para renderizar pagina completamente
-const express = require('express');
-const app = express();
-
-app.use(express.static('./'));  //Path del archivo estatico HTML
-
-const server = app.listen(8000, function () {
-  console.log('server is running at %s', server.address().port);
-});
