772deda3bd4d36062b0d47d8b12f4a6cfe469d81
Gonzalo <gonzalom97@outlook.com>
2020-08-06 22:50:54 -0300

    feat: Importacion del componente Header en la App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <!-- Header -->
+    <Header></Header>
 
-    <div class="app-container font-body">
+    <div class="app-container  font-body">
       
     </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
