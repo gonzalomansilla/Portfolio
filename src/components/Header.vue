aea8f2b2ce12aeb284c05826448d99378f7d9ef6
Gonzalo <gonzalom97@outlook.com>
2020-08-10 23:13:33 -0300

    refactor: Icons
    Se creo componente para la fila de iconos

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,63 +2,70 @@
   <header class="relative w-full h-screen font-header">
     <div class="header__img absolute top-0 w-full h-screen"></div>
 
-    <div class="header__text grid grid-rows-2 gap-8 h-screen">
-			<h1 class="text-5xl sm:text-6xl self-end text-center text-primary font-light tracking-wider leading-tight">
-				Hello<span class="text-white">, i'm
-        <br class="block md:hidden">
-        Johon Doe</span>
-			</h1>
-			<h1 class="text__profession self-start ss:w-auto px-12 py-2 sm:px-16 text-2xl sm:text-3xl text-center text-white">WEB DEVELOPER</h1>
-    </div>
+    <div class="header__content grid grid-rows-2 gap-8 h-screen">
+      <h1
+        class="text-5xl sm:text-6xl self-end text-center text-primary font-light tracking-wider leading-tight"
+      >
+        Hello
+        <span class="text-white">
+          , i'm
+          <br class="block md:hidden"/>Johon Doe
+        </span>
+      </h1>
 
-    <!-- <div class="header__social-icons social-icons icons_theme-white">
-      <i class="fab fa-twitter fa-2x social-icons__icon icon"></i>
-      <i class="fab fa-linkedin-in fa-2x social-icons__icon icon"></i>
-      <i class="fab fa-github fa-2x social-icons__icon icon"></i>
-      <i class="fab fa-stack-overflow fa-2x social-icons__icon icon"><a href=""></a></i>
-    </div> -->
+      <h1
+        class="relative self-start ss:w-auto px-12 py-1 sm:px-16 text-2xl sm:text-3xl text-center text-white"
+      >
+        WEB DEVELOPER
+      </h1>
+
+      <IconsList class="mb-12"></IconsList>
+    </div>
   </header>
 </template>
 
 <script>
-export default {
+import IconsList from "./ui/IconsList";
 
-}
+export default {
+  name: 'Header',
+  components: {
+    IconsList
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 /* DISEÑO */
 .header__img {
   z-index: -10;
-  background-image: url('./../../public/img/header-bg.jpg');
+  background-image: url("./../../public/img/header-bg.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   clip-path: polygon(0 0, 100% 0%, 100% calc(100% - 48px), 0 100%);
-  filter: brightness(.25);
+  filter: brightness(0.25);
 }
 
 .text__profession {
   &:before {
-      content: "";
-      background-color: theme('colors.primary');
-      opacity: theme('opacity.75');
-      clip-path: polygon(12% 0, 100% 0%, 88% 100%, 0% 100%);
+    content: "";
+    background-color: theme("colors.primary");
+    opacity: theme("opacity.75");
+    clip-path: polygon(12% 0, 100% 0%, 88% 100%, 0% 100%);
   }
 }
 
-
 /* LAYOUT */
-.header__text {
+.header__content {
   justify-items: center;
 }
 
 .header__img {
-	height: calc(100% + 48px);
+  height: calc(100% + 48px);
 }
 
 .text__profession {
-  position: relative;
   min-width: 264px;
 
   &::before {
@@ -71,9 +78,5 @@ export default {
     width: 100%;
     height: 100%;
   }
-}
-
-.header__social-icons {
-    // @include positioning(absolute, calc(100vh - 80px), 50%, translateX(-50%));
 }
 </style>
