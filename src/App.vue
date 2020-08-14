a93a14b3a99e802408db57393706e552b8a895f5
Gonzalo <gonzalom97@outlook.com>
2020-08-13 22:29:19 -0300

    feat: Componente AboutSection

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,23 @@
     <!-- Header -->
     <Header></Header>
 
-    <div class="app-container  font-body">
-      
+    <div class="app-container font-body">
+      <AboutSection></AboutSection>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import AboutSection from "./components/sections/AboutSection";
 
 export default {
   name: 'App',
   components: {
-    Header
-  }
-}
+    Header,
+    AboutSection
+  },
+};
 </script>
 
 <style src="./assets/tailwind.css"></style>
---
+++ src/components/sections/AboutSection.vue
@@ -0,0 +1,100 @@
+<template>
+  <AppSection section-title="About Me">
+    <template #section-content>
+      <div class="content">
+        <div class="about__img-perfil img-perfil">
+          <div class="img-perfil__container hidden">
+            <img src="img/optimized/perfil.jpg" alt="perfil_photo" />
+          </div>
+        </div>
+
+        <!-- Text -->
+        <div class="">
+          <h3 class="mb-2 text-xl font-light text-primary capitalize">who am i?</h3>
+          <h2 class="mb-2 text-xl font-bold capitalize">i'm  john doe, a web developer and  visual ux/ui designer</h2>
+          <p class="pb-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita odio
+            maiores, earum cupiditate ipsa recusandae pariatur aperiam enim
+            blanditiis animi iusto tempora aliquam reprehenderit eius autem.
+            Veniam odit minus rerum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            quidem delectus alias sit ipsum ducimus id, perspiciatis itaque
+            maiores commodi.
+          </p>
+        </div>
+
+        <hr class="bg-gray-400 my-8">
+
+        <!-- TODO More info -->
+
+        <!-- TODO Component -> Button primary -->
+        <div class="about__more-info">
+          <button class="bg-primary w-full text-white font-light px-4 py-2 rounded-md">
+            Dowload CV
+          </button>
+        </div>
+      </div>
+    </template>
+  </AppSection>
+</template>
+
+<script>
+import AppSection from "./AppSection";
+
+export default {
+  name: "AboutSection",
+  components: {
+    AppSection,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.img-perfil__container {
+  position: relative;
+}
+
+.img-perfil__container img{
+  width: 200px;
+  height: auto;
+}
+
+.img-perfil__container:before{
+  z-index: -1;
+  width: calc(100% + 32px);
+  height: calc(100% + 32px);
+  /* @include positioning(absolute, -16px, 50%, translateX(-50%)); */
+}
+
+// .about__more-info {
+//   display: flex;
+//   justify-self: right;
+// }
+
+/* Diseño */
+
+.img-perfil__container img{
+  object-fit: cover;
+  object-position: top;
+  /* box-shadow: $shadow-img; */
+}
+
+.img-perfil__container:before {
+  content: "";
+  /* background: $color-secundary; */
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 30%);
+}
+
+.perfil__subtitle {
+   /* font-size: $text-size__subtitle; */
+   /* color: $color-secundary;
+   font-weight: $font-weight__medium; */
+   /* letter-spacing: $letter-spacing__normal; */
+}
+
+.perfil__parraf {
+  /* color: $text-primary; */
+}
+</style>
