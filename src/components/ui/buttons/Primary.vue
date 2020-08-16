ebd89a9bcd0a0d1b46f6dfd99f83c804688951b6
Gonzalo <gonzalom97@outlook.com>
2020-08-15 22:38:25 -0300

    refact: Elimnacion div contenedor

--- src/components/ui/buttons/Primary.vue
+++ src/components/ui/buttons/Primary.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <BaseButton class="text-white bg-primary-50 hover:bg-primary-60">
-      <template #text>
-        {{ text }}
-      </template>
-    </BaseButton>
-  </div>
+  <BaseButton class="text-white bg-primary-50 hover:bg-primary-60">
+    <template #text>
+      {{ text }}
+    </template>
+  </BaseButton>
 </template> 
 
 <script>
