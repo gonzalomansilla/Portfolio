c5c37a030e0c4cb917580306081fd0c400b45291
Gonzalo <gonzalom97@outlook.com>
2020-08-15 21:17:57 -0300

    refact: Componente para boton primario

---
+++ src/components/ui/buttons/Primary.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <BaseButton class="text-white bg-primary-50 hover:bg-primary-60">
+      <template #text>
+        {{ text }}
+      </template>
+    </BaseButton>
+  </div>
+</template> 
+
+<script>
+import BaseButton from "./../../base/BaseButton";
+
+export default {
+  name: 'BtnPrimary',
+  components: {
+    BaseButton
+  },
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style>
+
+</style>
