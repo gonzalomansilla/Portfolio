eaba14005de46ac861018f1b177aede01e0da7f5
Gonzalo <gonzalom97@outlook.com>
2020-08-16 20:54:56 -0300

    feat: Componente ProyectTag

---
+++ src/components/ui/ProyectTag.vue
@@ -0,0 +1,74 @@
+<template>
+  <a class="font-light bg-gray-400 rounded-full" 
+    :class="[this.getTheme, this.getSize]"
+    href="#"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: "ProyectTag",
+  props: {
+    text: {
+      type: String,
+      default: "Text",
+    },
+    theme: {
+      type: String,
+      validator: (theme) => ['blue', 'white'].includes(theme),
+    },
+    size: {
+      type: String,
+      validator: (size) => ["small", "medium"].includes(size),
+    },
+  },
+  computed: {
+    getTheme() {
+      let theme = 'theme--';
+
+      if(this.theme == 'blue')
+        theme += this.theme;
+      else if(this.theme == 'white')
+        theme += this.theme;
+
+      return theme;
+    },
+    getSize() {
+      let size = 'size--';
+
+      if(this.size == 'small')
+        size += this.size;
+      else if(this.size == 'medium')
+        size += this.size;
+
+      return size
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// Themes
+.theme--white {
+  background: theme('colors.white');
+  color: theme('colors.primary-50');
+  box-shadow: theme('boxShadow.default');
+}
+
+.theme--blue {
+  background: theme('colors.primary-50');
+  color: theme('colors.white');
+}
+
+// Sizes
+.size--small {
+  font-size: theme('fontSize.sm');
+  padding: theme('spacing.2') theme('spacing.3');
+}
+
+.size--medium {
+  padding: theme('spacing.2') theme('spacing.5');
+}
+</style>
