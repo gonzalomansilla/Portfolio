5456c2d23a3bca38eb2a3fce50657bef4d2fa633
Gonzalo <gonzalom97@outlook.com>
2020-08-15 22:38:51 -0300

    feat: Componente LabelInfo

---
+++ src/components/ui/LabelInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="align-middle">
+    <span class="font-bold capitalize">{{ labelTitle }}: </span>
+    <span class="pl-1 capitalize opacity-75">{{ labelValue }}</span>
+    <slot name="special-text"></slot>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'LabelInfo',
+  props: {
+    labelTitle: {
+      type: String,
+      requiere: true,
+      default: 'Title'
+    },
+    labelValue: {
+      type: String,
+      requiere: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  width: max-content;
+}
+</style>
