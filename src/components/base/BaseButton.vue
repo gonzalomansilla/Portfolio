256371f51fdabe349aefbf35ee550fa6ab58f3bf
Gonzalo <gonzalom97@outlook.com>
2020-08-15 21:16:44 -0300

    refact: Componente base
    Componente base para los botones

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="w-full md:w-auto font-light px-8 py-2 rounded-md shadow-md">
+    <slot name="text"></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    text: {
+      type: String,
+      requiere: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
