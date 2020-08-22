72529466dfe4de4120fa91434f0edcc55d81164c
Gonzalo <gonzalom97@outlook.com>
2020-08-22 13:03:46 -0300

    feat: Grid de proyectos y componente Proyect

--- src/components/sections/Portfolio.vue
+++ src/components/sections/Portfolio.vue
@@ -12,195 +12,11 @@
           ></ProyectTag>
         </div>
 
-        <div class="proyects grid gap-4">
-          <!-- TODO Proyectos -->
-          <div>
-            PROYECTS CONTAINER
-            <!-- 
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img.jpg"
-                  loading="lazy"
-                  lt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas, amet consectetur adipisicing elit
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>
-
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img2.jpg"
-                  loading="lazy"
-                  alt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas.
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>
-
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img7.jpg"
-                  loading="lazy"
-                  alt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas.
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>
-
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img4.jpg"
-                  loading="lazy"
-                  alt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas.
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>
-
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img5.jpg"
-                  loading="lazy"
-                  alt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas.
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>
-
-            <div class="proyect">
-              <div class="proyect__img-container">
-                <img
-                  src="img/optimized/proyect-img6.jpg"
-                  loading="lazy"
-                  alt="Proyect image"
-                  class="proyect__img center-img"
-                />
-              </div>
-              <div class="proyect__details details">
-                <h3 class="details__title">Proyect Title</h3>
-                <p class="details__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia
-                  dolorum voluptas.
-                </p>
-                <div class="details__labels labels">
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>HTML</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>CSS</a>
-                  </div>
-                  <div class="labels__label label label_size_small label_theme_white">
-                    <a href>JavaScript</a>
-                  </div>
-                </div>
-                <a href class="details__link-github">GitHub</a>
-              </div>
-            </div>-->
-          </div>
-
-          <a class="uppercase">view more</a>
+        <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
+          <Proyect v-for="(proyect, index) in proyects" :key="index" :proyect="proyect"></Proyect>
         </div>
+
+        <a class="text-center uppercase">view more</a>
       </section>
     </template>
   </BaseSection>
@@ -209,12 +25,14 @@
 <script>
 import BaseSection from "./../base/BaseSection";
 import ProyectTag from "./../ui/ProyectTag";
+import Proyect from "./../ui/Proyect";
 
 export default {
   name: "PortfolioSection",
   components: {
     BaseSection,
     ProyectTag,
+    Proyect,
   },
   data: () => ({
     proyectTags: [
@@ -227,99 +45,58 @@ export default {
       "Spring Boot",
       "SQL Server",
     ],
+    proyects: [
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["Vue", "C#", "SQL Server"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img2.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["JavaScript"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img4.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["Java", "Spring Boot"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img5.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["C#", ".NET Core"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img6.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["Vue", "C#", ".NET Core"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+      {
+        title: "Proyect Title",
+        imageUrl: "/img/proyect-img7.jpg",
+        descript:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+        tecnologies: ["Java"],
+        repositoryUrl: "https://github.com/NewtonGrav",
+      },
+    ],
   }),
 };
 </script>
 
-<style lang="scss" scoped>
-.proyect {
-  &:hover > .proyect__details {
-    opacity: 1;
-  }
-}
-
-.proyect__img-container {
-  object-fit: cover;
-}
-
-.proyect__details {
-  opacity: 0;
-  transition: all 0.6s;
-}
-
-.details {
-  text-align: center;
-}
-
-.details__title {
-  text-transform: uppercase;
-}
-
-.center-img {
-  object-fit: cover;
-  height: 100%;
-}
-
-// LAYOUT
-.portfolio__title {
-  grid-area: title;
-}
-
-.proyect {
-  position: relative;
-}
-
-// Patron de diseño
-.proyect__img-container {
-  position: relative;
-  padding-top: 56.25%;
-  width: 100%;
-  overflow: hidden;
-}
-
-.proyect__img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-}
-
-.details {
-  display: grid;
-  grid-template-columns: 1fr auto;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-row-gap: 32px;
-  grid-column-gap: 32px;
-  grid-template-areas:
-    "title title"
-    "description description"
-    "labels link-github";
-
-  height: 100%;
-  width: 100%;
-  // height: calc(100% - 32px);
-  // width: calc(100% - 32px);
-  padding: 16px;
-}
-
-.details__title {
-  grid-area: title;
-  align-self: center;
-}
-
-.details__description {
-  grid-area: description;
-}
-
-.details__labels {
-  grid-area: labels;
-  justify-content: left;
-  align-self: flex-end;
-  // Parche: soluciona alineamiento con respecto al link de GitHub
-  margin-bottom: -4px;
-}
-
-.details__link-github {
-  align-self: flex-end;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/ui/Proyect.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="proyect relative h-64 shadow-sm rounded-md overflow-hidden border border-gray-200">
+    <div class="proyect__img-container">
+      <img
+        :src="proyect.imageUrl"
+        loading="lazy"
+        let="Proyect image"
+        class="proyect__img object-cover"
+      />
+    </div>
+
+    <div class="proyect__details md:hidden absolute inset-x-0 bottom-0 h-12 sm:h-16 sm:text-lg w-full bg-primary-50 opacity-85">
+      <div class="w-full h-full flex gap-4 justify-between items-center p-3 font-light text-white">
+        <p>{{ proyect.title }}</p>
+        <a :href="proyect.repositoryUrl">GitHub</a>
+      </div>
+    </div>
+
+    <div class="my__hover hidden md:hover:block absolute inset-0 bg-primary-50 bg-opacity-90">
+      <div class="w-full h-full grid grid-cols-1 items-center p-4 text-white">
+        <a :href="proyect.repositoryUrl" class="self-start text-lg text-center tracking-wider">{{ proyect.title }}</a>
+
+        <p class="text-center font-light tracking-wider">{{ proyect.descript }}</p>
+
+        <div class="flex justify-between self-end">
+          <div class="flex gap-2 ">
+            <ProyectTag
+              v-for="(tag, index) in proyect.tecnologies"
+              :key="index"
+              theme="white"
+              size="small"
+              :text="tag"
+            ></ProyectTag>
+          </div>
+
+          <a :href="proyect.repositoryUrl" class="self-end font-light">View proyect</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProyectTag from "./ProyectTag";
+
+export default {
+  name: "Proyect",
+  components: {
+    ProyectTag
+  },
+  props: {
+    proyect: {
+      title: String,
+      imageUrl: String,
+      descript: String,
+      tecnologies: [],
+      repositoryUrl: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@screen md {
+  .proyect:hover .my__hover{
+    display: block;
+  }
+}
+
+
+// .proyect__details-hover {
+//   opacity: 0;
+//   transition: all 0.6s;
+// }
+
+// LAYOUT
+
+// Patron de diseño
+.proyect__img-container {
+  position: relative;
+  padding-top: 56.25%;
+  width: 100%;
+  overflow: hidden;
+}
+
+.proyect__img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+// .details {
+//   display: grid;
+//   grid-template-columns: 1fr auto;
+//   grid-template-rows: 1fr 1fr 1fr;
+//   grid-row-gap: 32px;
+//   grid-column-gap: 32px;
+//   grid-template-areas:
+//     "title title"
+//     "description description"
+//     "labels link-github";
+
+//   height: 100%;
+//   width: 100%;
+//   // height: calc(100% - 32px);
+//   // width: calc(100% - 32px);
+//   padding: 16px;
+// }
+</style>
