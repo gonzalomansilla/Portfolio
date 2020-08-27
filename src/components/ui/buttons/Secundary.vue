71cadefdb0e1d2f7355ca645520b7320115aeaf1
Gonzalo <gonzalom97@outlook.com>
2020-08-26 23:10:37 -0300

    feat: Boton secundario

---
+++ src/components/ui/buttons/Secundary.vue
@@ -0,0 +1,25 @@
+<template>
+   <BaseButton class="text-primary-50 bg-white active:bg-gray-200">
+    <template #text>
+      {{ text }}
+    </template>
+  </BaseButton>
+</template>
+
+<script>
+import BaseButton from "./../../base/BaseButton";
+
+export default {
+  name: 'BtnSecundary',
+  components: {
+    BaseButton
+  },
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style>
+
+</style>
