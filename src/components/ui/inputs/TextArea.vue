703d4ecdd6df181a2a4c5988e00f529d0ec60218
Gonzalo <gonzalom97@outlook.com>
2020-08-26 23:10:50 -0300

    feat: Inputs

---
+++ src/components/ui/inputs/TextArea.vue
@@ -0,0 +1,17 @@
+<template>
+  <textarea class="pl-4 w-full h-32 font-normal capitalize rounded-md shadow-md focus:outline-none" :placeholder="placeholder"></textarea>
+</template>
+
+<script>
+export default {
+  name: "TextArea",
+  props: {
+    placeholder: {
+      type: String,
+      requiere: true,
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/ui/inputs/TextBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="text" class="pl-4 w-full h-12 text-base font-normal capitalize rounded-md shadow-md focus:outline-none" :placeholder="placeholder" />
+</template>
+
+<script>
+export default {
+  name: "TextBox",
+  props: {
+    placeholder: {
+      type: String,
+      requiere: true
+    }
+  }
+}
+</script>
+
+<style></style>
