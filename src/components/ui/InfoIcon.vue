89aaee7789848156e55702900cd226d382566f3e
Gonzalo <gonzalom97@outlook.com>
2020-08-28 20:45:20 -0300

    feat: Componente InfoIcon

---
+++ src/components/ui/InfoIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid grid-rows-2 gap-y-2 justify-items-center full-max-content text-white">
+    <i class="fas icon text-2xl" :class="faIcon"></i>
+    <span class="text-base">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoIcon",
+  props: {
+    faIcon: {
+      type: String,
+      requiere: true
+    },
+    text: {
+      type: String,
+      requiere: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.contact-option {
+  
+}
+</style>
