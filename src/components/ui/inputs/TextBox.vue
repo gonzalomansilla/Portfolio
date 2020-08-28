1d8862d4c94e0e33826edb976a3e057431b45471
Gonzalo <gonzalom97@outlook.com>
2020-08-28 20:14:08 -0300

    style: Inputs

--- src/components/ui/inputs/TextBox.vue
+++ src/components/ui/inputs/TextBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="text" class="p-2 w-full h-12 text-base font-normal capitalize rounded-md shadow-md focus:outline-none" :placeholder="placeholder" />
+  <input type="text" class="p-2 w-full h-12 text-base text-gray-700 font-normal capitalize rounded-md shadow-md focus:outline-none" :placeholder="placeholder" />
 </template>
 
 <script>
