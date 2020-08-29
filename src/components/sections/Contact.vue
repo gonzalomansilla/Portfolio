63fa4cbd685ce519c57ba49da49ac3454d3d8202
Gonzalo <gonzalom97@outlook.com>
2020-08-28 21:15:54 -0300

    feat: Opciones de contacto

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -1,10 +1,10 @@
 <template>
-  <BaseSection section-title="Contact Me">
+  <BaseSection section-title="contact me">
     <template #section-content>
       <div class="relative px-4 py-12">
         <div class="contact-bg absolute inset-0"></div>
 
-        <div class="grid grid-cols-1 grid-rows-2 gap-y-12 pt-8">
+        <div class="grid grid-cols-1 grid-rows-content gap-y-12 py-8">
           <div class="grid gap-y-4">
             <div class="grid gap-y-2">
               <TextBox placeholder="full name"></TextBox>
@@ -15,24 +15,10 @@
             <BtnSecundary text="Send Message"></BtnSecundary>
           </div>
 
-          <!-- TODO fix -->
-          <div class="contact__contact-options contact-options icons_theme-white">
-            <!-- TODO Componente InfoIcon  -->
-            <div class="contact-option">
-              <i class="fas fa-phone contact-option__icon icon"></i>
-              <span class="contact-option__title">Phone</span>
-              <span class="contact-option__text">(+54) 11-34563422</span>
-            </div>
-            <div class="contact-option">
-              <i class="fas fa-inbox contact-option__icon icon"></i>
-              <span class="contact-option__title">Email</span>
-              <span class="contact-option__text">johondoe@gmail.com</span>
-            </div>
-            <div class="contact-option">
-              <i class="fas fa-location-arrow contact-option__icon icon"></i>
-              <span class="contact-option__title">Location</span>
-              <span class="contact-option__text">Argentina, Buenos Aires</span>
-            </div>
+          <div class="contact-options grid grid-rows-content justify-items-center gap-y-6">
+            <InfoIcon faIcon="fa-phone" text="(+54) 11-12345678" ></InfoIcon>
+            <InfoIcon faIcon="fa-inbox" text="johondoe@example.com" ></InfoIcon>
+            <InfoIcon faIcon="fa-location-arrow" text="Buenos Aires, Argentina" ></InfoIcon>
           </div>
         </div>
       </div>
@@ -45,6 +31,7 @@ import BaseSection from "./../base/BaseSection";
 import BtnSecundary from "./../ui/buttons/Secundary";
 import TextBox from "./../ui/inputs/TextBox";
 import TextArea from "./../ui/inputs/TextArea";
+import InfoIcon from "./../ui/InfoIcon";
 
 export default {
   name: "ContactSection",
@@ -52,59 +39,24 @@ export default {
     BaseSection,
     BtnSecundary,
     TextBox,
-    TextArea
+    TextArea,
+    InfoIcon
   },
 };
 </script>
 
 <style lang="scss" scoped>
-/* Design */
 .contact-bg {
   z-index: -1;
   clip-path: polygon(0 48px, 100% 0, 100% calc(100% - 48px), 0 100%);
   background: theme("colors.primary-50");
 }
 
-.contact-option__text {
-  color: rgba(255, 255, 255, 0.8);
-  font-weight: 300;
-  font-size: 16px;
-}
-
-.contact-option__title {
-  font-weight: 300;
-  color: white;
-}
-
-/* Layout */
-.contact-options {
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-  grid-row-gap: 16px;
-  justify-content: flex-start;
-  align-self: center;
-}
-
-.contact-option {
-  display: grid;
-  grid-template-columns: repeat(2, auto);
-  grid-template-rows: repeat(2, auto);
-  grid-template-areas:
-    "contact-icon contact-title"
-    ".            contact-text";
-  grid-column-gap: 16px;
-  justify-self: left;
-}
-
-.contact-option__icon {
-  grid-area: contact-icon;
-}
-
-.contact-option__title {
-  grid-area: contact-title;
-}
-
-.contact-option__text {
-  grid-area: contact-text;
-}
+@screen md {
+  .contact-options {
+    grid-template-columns: repeat(3, minmax(0, max-content));
+    grid-gap: theme('gap.6');
+    justify-content: center;
+  }
+  }
 </style>
