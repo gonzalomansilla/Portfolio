514d39af119fd3e0f05e62f5128ecd706d759b35
Gonzalo <gonzalom97@outlook.com>
2020-08-30 22:17:19 -0300

    style: font weight

--- src/components/ui/InfoIcon.vue
+++ src/components/ui/InfoIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-rows-2 gap-y-2 justify-items-center full-max-content text-white">
     <i class="fas icon text-xl" :class="faIcon"></i>
-    <span class="text-base">{{ text }}</span>
+    <span class="text-base font-light">{{ text }}</span>
   </div>
 </template>
 
