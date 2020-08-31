db2c597b8498e7c55dd31510246e96ecb23eae64
Gonzalo <gonzalom97@outlook.com>
2020-08-30 22:26:21 -0300

    refact: apply de dstilo de inputs

--- src/components/ui/inputs/TextArea.vue
+++ src/components/ui/inputs/TextArea.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea class="p-2 w-full h-32 text-base text-gray-700 font-normal capitalize rounded-md shadow-md focus:outline-none" :placeholder="placeholder"></textarea>
+  <textarea class="app-input focus:outline-none" :placeholder="placeholder"></textarea>
 </template>
 
 <script>
