e4d30fcdc8fcd610cf6b7bce69f7f39491283545
Gonzalo <gonzalom97@outlook.com>
2020-09-02 21:18:28 -0300

    style: texto descripcion de proyecto hasta md

--- src/components/ui/Proyect.vue
+++ src/components/ui/Proyect.vue
@@ -9,9 +9,9 @@
     />
 
     <div class="proyect__details md:hidden absolute inset-x-0 bottom-0 h-12 sm:h-16 sm:text-lg w-full bg-primary-50 opacity-85">
-      <div class="w-full h-full grid gap-4 grid-cols-2 items-center p-3 font-light text-white">
-        <p class="font-normal tracking-wider truncate" >{{ proyect.title }}</p>
-        <a class="justify-self-end" :href="proyect.repositoryUrl">View proyect</a>
+      <div class="w-full h-full grid gap-x-2 grid-cols-2 items-center p-3 text-base font-light text-white">
+        <p class="tracking-wider truncate" >{{ proyect.title }}</p>
+        <a class="justify-self-end " :href="proyect.repositoryUrl">View proyect</a>
       </div>
     </div>
 
