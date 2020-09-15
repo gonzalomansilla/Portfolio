76dbbece3950b5840e2dd3712bc9e45499ee6e4f
Gonzalo <gonzalom97@outlook.com>
2020-09-15 16:41:22 -0300

    feat: Responsive via JS para tags

--- src/components/sections/Portfolio.vue
+++ src/components/sections/Portfolio.vue
@@ -7,7 +7,7 @@
             v-for="(tag, index) in proyectTags"
             :key="index"
             theme="blue"
-            size="small"
+            :size="isScreenXL ? 'medium' : 'small'"
             :text="tag"
           ></ProyectTag>
         </div>
@@ -36,68 +36,84 @@ export default {
     ProyectTag,
     Proyect,
   },
-  data: () => ({
-    proyectTags: [
-      "JavaScript",
-      "Vue",
-      "C#",
-      ".NET Core",
-      "Entity Framework",
-      "Java",
-      "Spring Boot",
-      "SQL Server",
-    ],
-    proyects: [
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["Vue", "C#"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img2.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["JavaScript"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img4.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["Java", "Spring Boot"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img5.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["C#", ".NET Core"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img6.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["Vue", ".NET Core"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-      {
-        title: "Proyect Title",
-        imageUrl: "/img/proyect-img7.jpg",
-        descript:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
-        tecnologies: ["Java"],
-        repositoryUrl: "https://github.com/NewtonGrav",
-      },
-    ],
-  }),
+  data: function () {
+    return {
+      isScreenXL: false,
+      proyectTags: [
+        "JavaScript",
+        "Vue",
+        "C#",
+        ".NET Core",
+        "Entity Framework",
+        "Java",
+        "Spring Boot",
+        "SQL Server",
+      ],
+      proyects: [
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["Vue", "C#"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img2.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["JavaScript"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img4.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["Java", "Spring Boot"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img5.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["C#", ".NET Core"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img6.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["Vue", ".NET Core"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+        {
+          title: "Proyect Title",
+          imageUrl: "/img/proyect-img7.jpg",
+          descript:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia dolorum voluptas, amet consectetur adipisicing elit.",
+          tecnologies: ["Java"],
+          repositoryUrl: "https://github.com/NewtonGrav",
+        },
+      ],
+    };
+  },
+  mounted () {
+    const mediaQueryXL = window.matchMedia("(min-width: 1280px)");
+
+    mediaQueryXL.addListener(this.machScreen);
+
+    this.machScreen(mediaQueryXL);
+  },
+  methods: {
+    machScreen (e) {
+      if (e.matches) this.isScreenXL = true
+      else this.isScreenXL = false
+    }
+  },
 };
 </script>
 
